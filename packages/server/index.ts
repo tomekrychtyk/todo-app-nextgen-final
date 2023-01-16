0134bead95502caa025fb70ce03f8bad04b01dbb
Tomek Rychtyk <tomek@rychtyk.pl>
2023-01-16 15:44:47 +0100

    Update port

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -103,7 +103,7 @@ app.patch('/todo', async (req: Request, res: Response) => {
 //   res.send('success');
 // });
 
-const port = process.env.PORT || 8080;
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log('App is running on port:' + port);
 });
